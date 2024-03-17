c0800e58fd832a9c8a18c4804e0c8afef65fd648
mohamedahmed <alkomalymoahmed@gmail.com>
2024-03-17 04:46:39 +0200

    Add weather app project

--- src/CardsData.js
+++ src/CardsData.js
@@ -12,8 +12,17 @@ import Speech from "./Components/images/speech.png"
 import intr from "./Components/images/Interior Design.png"
 import login from "./Components/images/Screenshot (133).png"
 import Figma from "./Components/images/Figmaland.png"
+import weather from "./Components/images/weather.png"
 
 const CardsData = [
+    {
+        href: "https://mohamedahmed2023.github.io/Islamic-website/index.html",
+        type: "Pure",
+        "id": 2,
+        "title": "Islamic Website",
+        image:
+            Isl,
+    },
     {
         href: "https://github.com/MohamedAhmed2023/Crud-operations-reactjs",
         type: "React",
@@ -25,32 +34,9 @@ const CardsData = [
         ,
     },
     {
-        href: "https://mohamedahmed2023.github.io/Islamic-website/index.html",
-        type: "JS",
-        "id": 2,
-        "title": "Islamic Website",
-        image:
-            Isl,
-    },
-    {
-        href: "https://mohamedahmed2023.github.io/E-learning-Website/",
-        type: "JS",
-        "id": 3,
-        "title": "E-learning Website",
-        image: Ele
-        ,
-    },
-    {
-        href: "https://mohamedahmed2023.github.io/sina-university/",
-        type: "JS",
-        "id": 4,
-        "title": "sinai university",
-        image: Sinai,
-    },
-    {
         href: "https://e-commerce-dpe.netlify.app/",
         type: "React",
-        "id": 5,
+        "id": 3,
         "title":
             "e-commerce website"
         ,
@@ -60,25 +46,48 @@ const CardsData = [
     {
         href: "https://figmalandwebsite.netlify.app/",
         type: "React",
-        "id": 6,
+        "id": 4,
         "title":
             "Figma land website"
         ,
         image: Figma
         ,
     },
+    {
+        href: "https://weather-app-nodejs-2.onrender.com",
+        type: "NodeJs",
+        "id": 5,
+        "title": "Weather App",
+        image: weather,
+    },
+    {
+        href: "https://mohamedahmed2023.github.io/E-learning-Website/",
+        type: "Pure",
+        "id": 6,
+        "title": "E-learning Website",
+        image: Ele
+        ,
+    },
+    {
+        href: "https://mohamedahmed2023.github.io/sina-university/",
+        type: "Pure",
+        "id": 7,
+        "title": "sinai university",
+        image: Sinai,
+    },
+
 
     {
         href: "https://mohamedahmed2023.github.io/Bootstrap-website/",
-        type: "HTML & CSS",
-        "id": 7,
+        type: "Pure",
+        "id": 8,
         "title": "Bootstrap Website",
         image: Log,
     },
     {
         href: "https://mohamedahmed2023.github.io/Text-To-Speech-Generator/",
-        type: "JS",
-        "id": 8,
+        type: "Pure",
+        "id": 9,
         "title":
             "Text To Speech Generator"
         ,
@@ -88,51 +97,50 @@ const CardsData = [
 
     {
         href: "https://mohamedahmed2023.github.io/Live-Code-Editor/",
-        type: "JS",
-        "id": 9,
+        type: "Pure",
+        "id": 10,
         "title": "Live code editor",
         image: Liv
         ,
     },
     {
         href: "https://mohamedahmed2023.github.io/Javascript-Age-Calculator/",
-        type: "JS",
-        "id": 10,
+        type: "Pure",
+        "id": 11,
         "title": "Javascript Age Calculator",
         image: Age
         ,
     },
     {
         href: "https://mohamedahmed2023.github.io/Todo-App/",
-        type: "JS",
-        "id": 11,
+        type: "Pure",
+        "id": 12,
         "title": "Todo App",
         image: Todo
         ,
     },
     {
         href: "https://mohamedahmed2023.github.io/QR-Code-Generator/",
-        type: "JS",
-        "id": 12,
+        type: "Pure",
+        "id": 13,
         "title": "QR Code Generator",
         image: Qr
         ,
     },
     {
         href: "https://mohamedahmed2023.github.io/Interior-Design/",
-        type: "HTML & CSS",
-        "id": 13,
+        type: "Pure",
+        "id": 14,
         "title": "Interior Design",
         image: intr,
     },
     {
         href: "https://mohamedahmed2023.github.io/intern2grow-login-page/",
-        type: "HTML & CSS",
-        "id": 14,
+        type: "Pure",
+        "id": 15,
         "title": "Login Page",
         image: login,
     },
-
 
 ]
 
