e5b36d0a204d5469ada01759eb57b3694c628b8c
mohamedahmed <alkomalymoahmed@gmail.com>
2024-04-03 01:47:25 +0200

    add email.js to Form

--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -1,28 +1,52 @@
-import "./ContactUs.css"
+import React from 'react';
+import emailjs from 'emailjs-com';
+import Swal from 'sweetalert2';
+import "./ContactUs.css";
+
 function ContactUs() {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_z019xvl', 'template_4aq33pm', e.target, 'U92TN4oBo-MyFhWD_')
+            .then((result) => {
+                console.log(result.text);
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Success!',
+                    text: 'Your message has been sent successfully.',
+                });
+                e.target.reset();
+            }, (error) => {
+                console.log(error.text);
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Something went wrong! Please try again later.',
+                });
+            });
+    };
+
     return (
-        <>
-            <form id="contactSection">
-                <div class="form-container">
-                    <form class="form">
-                        <div class="form-group">
-                            <label for="Name">Name</label>
-                            <input required="" name="email" id="email" type="text" />
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input required="" name="email" id="email" type="text" />
-                        </div>
-                        <div class="form-group">
-                            <label for="textarea">How Can We Help You?</label>
-                            <textarea required="" cols="50" rows="10" id="textarea" name="textarea"></textarea>
-                        </div>
-                        <button type="submit" class="form-submit-btn">Submit</button>
-                    </form>
-                </div>
-            </form>
-        </>
-    )
+        <div id="contactSection">
+            <div className="form-container">
+                <form className="form" onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input required name="to_name" id="name" type="text" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input required name="from_name" id="email" type="email" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="message">Message</label>
+                        <textarea required cols="50" rows="10" id="message" name="message"></textarea>
+                    </div>
+                    <button type="submit" className="form-submit-btn">Submit</button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default ContactUs;
