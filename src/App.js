9c06fcc43caee821e8355cb14bf0df2f50879c4a
mohamedahmed <alkomalymoahmed@gmail.com>
2024-05-08 14:37:19 +0300

    delete img from folder

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
         />
         <Card item={item} />
         <div className="container-Contact us">
-          <HeadingText title="Contact us" />
+          <HeadingText title="Contact me" />
           <ContactUs />
         </div>
         <div className='container-Footer'>
