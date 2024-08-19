9597e25cc7e19fdbaf1f6fa7a825e2fa96d5f9af
mohamedahmed <alkomalymoahmed@gmail.com>
2024-08-19 21:48:58 +0300

    update fonts

--- src/Components/experience/Experience.js
+++ src/Components/experience/Experience.js
@@ -26,9 +26,9 @@ function Experience() {
                     <p>Front end developer internship provided by the NilePreneurs and Nile
                         University throughout the internship duration, we work develop Admin Panel
                         byReactJs for Nile University</p>
-                    <a href={NileCert} download="Nile-Certificate">
+                    {/* <a href={NileCert} download="Nile-Certificate">
                         <button className="cert-btn" type="button">Certificate</button>
-                    </a>
+                    </a> */}
                 </div>
                 <div data-aos="flip-left" className="experience-right">
                     <img src={dpe} alt=""></img>
@@ -37,9 +37,9 @@ function Experience() {
                     <p>Remotely</p>
                     <p>Front end developer internship provided by the Digital Pathways For Employment - DPE During the training period, I took on many tasks, and we created a personal portfolio and the final project.</p>
 
-                    <a href={DpeCert} download="DPE-Certificate">
+                    {/* <a href={DpeCert} download="DPE-Certificate">
                         <button className="cert-btn" type="button">Certificate</button>
-                    </a>
+                    </a> */}
                 </div>
             </div>
         </>
