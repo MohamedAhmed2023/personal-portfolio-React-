cf47b9f0db36ad79adf203cec1181e8afc25a266
mohamedahmed <alkomalymoahmed@gmail.com>
2024-09-14 01:44:26 +0300

    update Nav Bar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,60 +1,95 @@
-import React from "react";
-import "./NavBar.css"
+import React, { useState } from "react";
+import "./NavBar.css";
 import FacebookIcon from '@mui/icons-material/Facebook';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import WidgetsIcon from '@mui/icons-material/Widgets';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 import 'animate.css/animate.min.css';
 
 function NavBar() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
     return (
         <>
-            <nav class="navbar navbar-expand-lg animate__animated animate__fadeInDown animate__slow">
-                <div class="container-fluid">
+            <nav className="navbar navbar-expand-lg animate__animated animate__fadeInDown animate__slow">
+                <div className="container-fluid">
                     <div className="NavLogo">
-                        <a class="navbar-brand" href="/">Mohamed Alkomali</a>
+                        <a className="navbar-brand" href="/">Mohamed Alkomali</a>
                     </div>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <WidgetsIcon />
+                    <button className="navbar-toggler" type="button" onClick={toggleSidebar}>
+                        {isSidebarOpen ? <CloseIcon /> : <MenuIcon />}
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/#">Home</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/#">Home</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#AboutSection">About</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#AboutSection">About</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#experiencesection">experience</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#experiencesection">Experience</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#ProjectSection">Projects</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#ProjectSection">Projects</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#contactSection">Contact</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#contactSection">Contact</a>
                             </li>
                         </ul>
                         <div className="icon-navbar">
                             <a href="https://github.com/MohamedAhmed2023">
                                 <GitHubIcon />
                             </a>
-                            <a href="https://www.linkedin.com/in/mohamed-alkomali/" alt="">
+                            <a href="https://www.linkedin.com/in/mohamed-alkomali/">
                                 <LinkedInIcon />
                             </a>
-                            <a href="https://www.facebook.com/profile.php?id=100007754079369" alt="">
+                            <a href="https://www.facebook.com/profile.php?id=100007754079369">
                                 <FacebookIcon />
                             </a>
-                            <a href="https://www.instagram.com/mohamed_alkomali/" alt="">
+                            <a href="https://www.instagram.com/mohamed_alkomali/">
                                 <InstagramIcon />
                             </a>
                         </div>
                     </div>
                 </div>
             </nav>
+
+            <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+                <div className="NavLogoSideBar">
+                    <a className="navbar-brand" href="/">Mohamed Alkomali</a>
+                </div>
+                <ul className="sidebar-nav">
+                    <li><a href="/#">Home</a></li>
+                    <li><a href="#AboutSection">About</a></li>
+                    <li><a href="#experiencesection">Experience</a></li>
+                    <li><a href="#ProjectSection">Projects</a></li>
+                    <li><a href="#contactSection">Contact</a></li>
+                </ul>
+                <div className="icon-navbar">
+                    <a href="https://github.com/MohamedAhmed2023">
+                        <GitHubIcon />
+                    </a>
+                    <a href="https://www.linkedin.com/in/mohamed-alkomali/">
+                        <LinkedInIcon />
+                    </a>
+                    <a href="https://www.facebook.com/profile.php?id=100007754079369">
+                        <FacebookIcon />
+                    </a>
+                    <a href="https://www.instagram.com/mohamed_alkomali/">
+                        <InstagramIcon />
+                    </a>
+                </div>
+            </div>
+            {isSidebarOpen && <div className="overlay" onClick={toggleSidebar}></div>}
         </>
-    )
+    );
 }
 
 export default NavBar;
